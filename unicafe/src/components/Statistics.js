e10e0be172294c6af5414795a6bcae43cccc6669
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-03-15 03:25:16 +0530

    task: Add conditional rendering for stats

--- unicafe/src/components/Statistics.js
+++ unicafe/src/components/Statistics.js
@@ -2,15 +2,24 @@ const Statistics = ({good, neutral, bad}) => {
     const all = good + neutral + bad
     const average = (good - bad) / 9;
     const positive = good / all * 100;
+
     return (
         <div>
             <h1>statistics</h1>
-            <div>good {good}</div>
-            <div>neutral {neutral}</div>
-            <div>bad {bad}</div>
-            <div>all {all}</div>
-            <div>average {average}</div>
-            <div>positive {positive} %</div>
+            {
+                (all === 0)
+                ? (<div>No feedback given</div>)
+                : (
+                    <div>
+                        <div>good {good}</div>
+                        <div>neutral {neutral}</div>
+                        <div>bad {bad}</div>
+                        <div>all {all}</div>
+                        <div>average {average}</div>
+                        <div>positive {positive} %</div>
+                    </div>
+                )
+            }
         </div>
     )
 }
